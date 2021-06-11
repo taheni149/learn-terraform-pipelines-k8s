cb4341b5686c0474ae856c4bb9fb53885e54d23c
taheni149 <60134796+taheni149@users.noreply.github.com>
2021-06-11 10:20:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "remote" {
     organization = "infrastructure-pipelines-workshop"
     workspaces {
-      name = "john-d-k8s"
+      name = "judith-m-k8s"
     }
   }
   required_providers {
